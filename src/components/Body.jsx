a9bb8f0db01b032e5c9c6a16853dca9fecd58518
Aman <avman1998@gmail.com>
2024-04-13 17:16:36 +0530

    added top rated filter

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,12 +1,23 @@
 import RestaurantCard from "./RestaurantCard";
 import { restaurants } from "../config";
+import { useState } from "react";
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(restaurants);
+  function topRatedRes() {
+    const filteredRestaurants = listOfRestaurants.filter(
+      (res) => res?.info?.avgRating > 4
+    );
+    setListOfRestaurants(filteredRestaurants);
+  }
   return (
-    <div className="border border-black my-2 flex flex-wrap justify-between items-center">
-      {restaurants?.map((res) => (
-        <RestaurantCard key={res?.info?.id} restaurant={res} />
-      ))}
-    </div>
+    <>
+      <button onClick={topRatedRes}>Top rated restuarants</button>
+      <div className="border border-black my-2 flex flex-wrap justify-between items-center">
+        {listOfRestaurants?.map((res) => (
+          <RestaurantCard key={res?.info?.id} restaurant={res} />
+        ))}
+      </div>
+    </>
   );
 };
 

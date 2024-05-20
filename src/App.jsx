650f94bc126897b41624e8063b829ca1e8db6059
Aman <avman1998@gmail.com>
2024-05-20 23:26:12 +0530

    Added useOnline hook and useRestaurantMenu hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import Header from "./components/Header";
 import { Outlet } from "react-router-dom";
+import useOnline from "./custom-hooks/useOnline";
+
 function App() {
+  const isOnline = useOnline();
+  console.log("IsOnline", isOnline);
+  if (!isOnline) return <h1>Check your internet connection.</h1>;
   console.log("Outlet", Outlet);
   return (
     <div className="p-3">
--- src/custom-hooks/useRestaurantMenu.js
+++ src/custom-hooks/useRestaurantMenu.js
@@ -5,9 +5,7 @@ export default function useRestaurantMenu(res_id) {
   async function fetchData() {
     const res = await fetch(menu_url(res_id));
     const data = await res.json();
-    // setRestaurantData(data?.data?.cards[2]?.card?.card?.info);
     setRestaurantData(data?.data?.cards);
-    console.log("Res data", data?.data?.cards);
   }
   useEffect(() => {
     fetchData();
